b487c7698fddfe97e8475e2b590ed0f3d4e17551
nlaveck <nlaveck@gmail.com>
2023-12-17 21:20:50 -0500

    Day 12: Part 2

--- 2023/day12/12.py
+++ 2023/day12/12.py
@@ -1,16 +1,18 @@
 import sys, re
 
 def current(springs: str):
-    x = [len(x) for x in re.split('\.|\?',springs) if x]
+    x = [len(x) for x in re.split('\\.|\\?',springs) if x]
     return x
 
 def solve(springs: str, expected: list[int]):
-    #print(springs)
     actual = current(springs)
+    if len(actual) >= 4 and actual[:4] == [1,1,3,1]:
+        print(springs, actual)
+        return 0
     if actual == expected:
         return 1
-    if actual and max(actual) > max(expected) or sum(actual) > sum(expected):
-        return 0
+    # if  sum(actual) > sum(expected):
+    #     return 0
     
     mutable = [c for c in springs]
     
@@ -20,17 +22,20 @@ def solve(springs: str, expected: list[int]):
         operational = ''.join(mutable)
         mutable[idx] = '#'
         damaged = ''.join(mutable)
+        #print(operational)
         return solve(operational, expected) + solve(damaged, expected)
     else:
-        return 0
+        return actual == expected
 
 
 with open(sys.argv[1]) as in_file:
     points = 0
     while line := in_file.readline().strip():
         springs, expected = line.split()
-        expected = [int(x) for x in expected.split(',')]
+        expected = [int(x) for x in expected.split(',')] * 5
+        springs *= 5
         combo = solve(springs, expected)
+        break
         #print(springs, expected, combo)
         points += combo
     print(points)
--- 2023/day12/12_2.py
+++ 2023/day12/12_2.py
@@ -2,63 +2,53 @@ from functools import cache
 import sys, re
 
 def current(springs: str):
-    x = [len(x) for x in re.split('\.|\?',springs) if x]
+    x = [len(x) for x in re.split('\\.|\\?',springs) if x]
     return x
 
-def firstCount(springs: str):
-    if '#' in springs:
-        idx = springs.index('#')
-        count = 1
-        while idx + count < len(springs) and springs[idx+count] == '#':
-            count += 1
-        return count
+@cache
+def solve(springs: str, expected_idx):
+    
+    expected_slice = expected[expected_idx:]
+    #print(springs, expected)
+    actual = current(springs)
+
+    if actual == expected_slice:
+        return 1
+    if  sum(actual) > sum(expected_slice):
+        return 0
+
+    if '?' in springs:
+        idx = springs.index('?')
+
+        first_half = springs[:idx]
+        second_half = springs[idx+1:]
+
+        operational = '.' + second_half
+        damaged = '#' + second_half
+
+        solved = current(first_half)
+        prefix = ''
+
+        #if first half ends with '#'
+        if first_half and first_half[-1] == '#':
+            prefix = '#' * solved[-1]
+            solved = solved[:-1]
+            
+        if any(solved[i] != expected_slice[i] for i in range(len(solved))):
+            return 0
+        return solve(prefix + operational, expected_idx+len(solved)) + solve(prefix + damaged, expected_idx+len(solved))
     else:
         return 0
-
-
-    
-
-
-def solve(springs: str, expected: list[int]):
-
-    # i = position in springs
-    # j = position in expected
-    @cache
-    def dp(count, i, j):
-
-        if i == len(springs):
-            return 0
-        
-        new_springs = springs[i:]
-        if j == len(expected):
-            return 0 if '#' in new_springs else 1
-        
-        new_expected = expected[j:]
-
-        if count == new_expected[0]:
-            return dp(0, i, j+1) if springs[i] != '#' else 0
-
-
-        for i2, v in enumerate(new_springs):
-            if v == '?':
-                return dp(count+1, i+i2+1, j) + dp(count, i+i2+1, j)
-            if v == '#':
-                return dp(count+1, i+i2+1, j)
-        return 0
-
-    return dp(0,0,0)
-
-
-
 
 with open(sys.argv[1]) as in_file:
     points = 0
     while line := in_file.readline().strip():
         springs, expected = line.split()
-        expected = [int(x) for x in expected.split(',')]
-        springs *= 5
-        expected *= 5
-        combo = solve(springs, expected)
-        print(springs, expected, combo)
+        expected = [int(x) for x in expected.split(',')] * 5
+        springs = '?'.join([springs]*5)
+
+        combo = solve(springs, 0)
+        #print(combo)
         points += combo
+
     print(points)
